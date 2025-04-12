357df0af488c104b6d973023ed6aa91278ac250b
tomazcerne <tomaz.cerne03@gmail.com>
2025-04-12 16:47:04 +0200

    rides service

--- src/main/java/com/dragonhack/pejmo/services/RideService.java
+++ src/main/java/com/dragonhack/pejmo/services/RideService.java
@@ -3,7 +3,9 @@ package com.dragonhack.pejmo.services;
 import com.dragonhack.pejmo.dtos.RideInput;
 import com.dragonhack.pejmo.exceptions.resource_not_found.ResourceNotFoundException;
 import com.dragonhack.pejmo.models.Ride;
+import com.dragonhack.pejmo.models.User;
 import com.dragonhack.pejmo.repositories.RideRepository;
+import com.dragonhack.pejmo.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +13,11 @@ import java.util.List;
 @Service
 public class RideService {
     private final RideRepository rideRepository;
+    private final UserRepository userRepository;
 
-    public RideService(RideRepository rideRepository) {
+    public RideService(RideRepository rideRepository, UserRepository userRepository) {
         this.rideRepository = rideRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Ride> getAllRides() {
@@ -30,7 +34,16 @@ public class RideService {
     }
 
     public Ride createRide(RideInput rideInput) {
+        User user = userRepository.findById(rideInput.userId()).
+                orElseThrow(() -> new ResourceNotFoundException("User not found"));
         Ride ride = new Ride();
+        ride.setUser(user);
+        ride.setfromLocation(rideInput.fromLocation());
+        ride.settoLocation(rideInput.toLocation());
+        ride.setStartTime(rideInput.startTime());
+        ride.setPrice(rideInput.price());
+        ride.setFreeSeats(rideInput.freeSeats());
+        rideRepository.save(ride);
         return ride;
     }
 
